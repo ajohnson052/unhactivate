643a88f46460087e8b042e140762283e64929079
Alexis Johnson <alexis.johnson052@gmail.com>
2016-01-21 16:00:22 -0500

    models and migrations

---
+++ app/models/breach.rb
@@ -0,0 +1,3 @@
+class Breach < ActiveRecord::Base
+  belongs_to :organization
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160121203935) do
+ActiveRecord::Schema.define(version: 20160121205815) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "breaches", force: :cascade do |t|
+    t.string   "title"
+    t.date     "breach_date"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "organizations", force: :cascade do |t|
+    t.string   "name"
+    t.string   "domain"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false

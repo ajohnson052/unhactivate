2e0303176c17e8eb1710529163865cdf73f7daa9
James Moon <mooniker@gmail.com>
2016-01-22 20:27:18 -0500

    Add controllers for breaches and orgs, index and show views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'organizations/index'
+
+  get 'breaches/index'
+
+  resources :breaches
+  resources :organizations # we may not need this
+
   devise_for :users
   root to: "welcome#index"
   # The priority is based upon order of creation: first created -> highest priority.

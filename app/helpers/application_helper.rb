5b08bf6d5acfe403ee27366c7d7f6ba9380b08a0
James Moon <mooniker@gmail.com>
2016-01-25 23:34:26 -0500

    Dynamically allow Bootstrap nav to reflect current page with active class

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,4 +12,14 @@ module ApplicationHelper
     @devise_mapping ||= Devise.mappings[:user]
   end
 
+  # technique explained here: http://stackoverflow.com/questions/17481812/dynamically-add-active-class-to-bootstrap-li-in-rails
+  # <li class="acttive"><a href="#">Unhactivate <span class="sr-only">(current)</span></a></li>
+  # automatically sets class as "active" if the url target is current page
+  def nav_li(linked_text, link_path, html_options = {})
+    active = "active" if current_page?(link_path)
+    content_tag :li, class: active do
+      link_to linked_text, link_path, html_options
+    end
+  end
+
 end

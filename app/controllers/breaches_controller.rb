3e708c19b42c89c59c5cb4dd7981192a70af9c75
James Moon <mooniker@gmail.com>
2016-01-26 16:09:22 -0500

    Start stub for admin view, comment out unneeded JSON rendering

--- app/controllers/breaches_controller.rb
+++ app/controllers/breaches_controller.rb
@@ -17,10 +17,10 @@ class BreachesController < ApplicationController
     respond_to do |format|
       if @breach.save
         format.html { redirect_to @breach, notice: 'Breach was successfully created.' }
-        format.json { render :show, status: :created, location: @breach }
+        # format.json { render :show, status: :created, location: @breach }
       else
         format.html { render :new }
-        format.json { render json: @breach.errors, status: :unprocessable_entity }
+        # format.json { render json: @breach.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -33,10 +33,10 @@ class BreachesController < ApplicationController
     respond_to do |format|
       if @breach.update(breach_params)
         format.html { redirect_to @breach, notice: 'Breach was successfully updated.' }
-        format.json { render :show, status: :ok, location: @breach }
+        # format.json { render :show, status: :ok, location: @breach }
       else
         format.html { render :edit }
-        format.json { render json: @breach.errors, status: :unprocessable_entity }
+        # format.json { render json: @breach.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -49,6 +49,6 @@ class BreachesController < ApplicationController
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def breach_params
-    params.require(:breach).permit(:title, :breach_date, :description)
+    params.require(:breach).permit(:title, :breach_date, :description, :organization_id)
   end
 end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,8 +3,6 @@ class WelcomeController < ApplicationController
     @recent_breaches = Breach.order(breach_date: :desc)[0..5]
   end
 
-  def create
-    @user = User.new(params[:user])
-    UserMailer.welcome_user(@user).deliver_now
+  def admin
   end
 end

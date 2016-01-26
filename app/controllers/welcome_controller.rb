fa97aaee6f755d796fc15fc8207eed2ed53a7516
James Moon <mooniker@gmail.com>
2016-01-26 17:01:12 -0500

    Add nonworking stub for an admin page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,5 +4,11 @@ class WelcomeController < ApplicationController
   end
 
   def admin
+    if authenticate_user! && current_user.admin? then
+      @recent_breaches = Breach.order(breach_date: :desc)[0..10]
+    else
+      raise ActionController::RoutingError.new('Not Found')
+    end
+
   end
 end

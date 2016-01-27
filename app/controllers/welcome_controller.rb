121ec845853dceaee9dedaa9c5e36c332f8693c5
James Moon <mooniker@gmail.com>
2016-01-27 17:22:23 -0500

    Add breach partials to welcome page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,7 @@
 class WelcomeController < ApplicationController
 
   def index
-    @recent_breaches = Breach.order(breach_date: :desc)[0..5]
+    @recent_breaches = Breach.order(breach_date: :desc)[0..3]
     @articles = Article.order(created_at: :desc)[0..3]
   end
 

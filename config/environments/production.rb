abd34b44b25e126066bda3f07f7ca982b5afafd5
James Moon <mooniker@gmail.com>
2016-01-28 16:47:03 -0500

    Try changing config.action_mailer.default_url_options

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,17 +78,17 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   #Updated Action mailer
-  config.action_mailer.default_url_options = { host: 'http://unhactivate.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'https://unhackdemo.herokuapp.com' }
 
-config.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :smtp
 
-config.action_mailer.smtp_settings = {
-address: "smtp.gmail.com",
-port: 587,
-domain: ENV["GMAIL_DOMAIN"],
-authentication: "plain",
-enable_starttls_auto: true,
-user_name: ENV["GMAIL_USERNAME"],
-password: ENV["GMAIL_PASSWORD"]
-}
+  config.action_mailer.smtp_settings = {
+  address: "smtp.gmail.com",
+  port: 587,
+  domain: ENV["GMAIL_DOMAIN"],
+  authentication: "plain",
+  enable_starttls_auto: true,
+  user_name: ENV["GMAIL_USERNAME"],
+  password: ENV["GMAIL_PASSWORD"]
+  }
 end

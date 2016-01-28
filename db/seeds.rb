065dc842a10fae52dc3fffea35106b4efca3b8d5
James Moon <mooniker@gmail.com>
2016-01-28 11:28:01 -0500

    Add companies and categories seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,15 +6,18 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-file_name = File.expand_path(File.join(File.dirname(__FILE__), 'breach_data.json'))
-breach_data = JSON.parse(File.read(file_name))
-
 # Uncomment these to wipe out old data in the database
 Breach.delete_all
 Interest.delete_all
 Organization.delete_all
 Category.delete_all
 Article.delete_all
+
+
+# haveibeenpwned.com data
+
+file_name = File.expand_path(File.join(File.dirname(__FILE__), 'breach_data.json'))
+breach_data = JSON.parse(File.read(file_name))
 
 (1..7).each do |i| # generate 7 generic numbered categories
    Category.create(title: "Category#{i}")
@@ -52,3 +55,9 @@ end
   <p>But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?</p>")
 
 end
+
+
+cats = [
+  'Entertainment & News',
+
+]

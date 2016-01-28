529fe3bf013ae4ce6ecb112773e9361d98a15004
James Moon <mooniker@gmail.com>
2016-01-28 15:44:04 -0500

    Replace working seed data with demo seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,39 +16,39 @@ Article.delete_all
 
 # haveibeenpwned.com data
 
-file_name = File.expand_path(File.join(File.dirname(__FILE__), 'breach_data.json'))
-breach_data = JSON.parse(File.read(file_name))
-
-(1..7).each do |i| # generate 7 generic numbered categories
-   Category.create(title: "Category#{i}")
-end
-
-last_category = 0
-
-breach_data.each do |breach|
-
-  last_category += 1 # iterate through each category
-  if last_category > 7 then last_category = 1 end
-
-  cat = Category.find_by(title: "Category#{last_category}")
-
-  cat.organizations.create(
-    domain: breach["Domain"],
-    name: breach["Name"],
-  )
-
-  Organization.last.breaches.create(
-    # name: breach["Name"], # omitted
-    title: breach["Title"],
-    # domain: breach["Domain"], # in organization
-    breach_date: breach["BreachDate"],
-    # added_date: breach["AddedDate"], # omitted
-    # pwn_count: breach["PwnCount"], # omitted
-    description: breach["Description"],
-    # is_sensitive: breach["IsSensitive"] # omitted
-  )
-
-end
+# file_name = File.expand_path(File.join(File.dirname(__FILE__), 'breach_data.json'))
+# breach_data = JSON.parse(File.read(file_name))
+#
+# (1..7).each do |i| # generate 7 generic numbered categories
+#    Category.create(title: "Category#{i}")
+# end
+#
+# last_category = 0
+#
+# breach_data.each do |breach|
+#
+#   last_category += 1 # iterate through each category
+#   if last_category > 7 then last_category = 1 end
+#
+#   cat = Category.find_by(title: "Category#{last_category}")
+#
+#   cat.organizations.create(
+#     domain: breach["Domain"],
+#     name: breach["Name"],
+#   )
+#
+#   Organization.last.breaches.create(
+#     # name: breach["Name"], # omitted
+#     title: breach["Title"],
+#     # domain: breach["Domain"], # in organization
+#     breach_date: breach["BreachDate"],
+#     # added_date: breach["AddedDate"], # omitted
+#     # pwn_count: breach["PwnCount"], # omitted
+#     description: breach["Description"],
+#     # is_sensitive: breach["IsSensitive"] # omitted
+#   )
+#
+# end
 
 (1..10).each do |i|
   Article.create(title: "Article#{i}", body: "<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
@@ -57,7 +57,20 @@ end
 end
 
 
-cats = [
-  'Entertainment & News',
+file_name = File.expand_path(File.join(File.dirname(__FILE__), 'companies_categories_Jan28.json'))
+companies = JSON.parse(File.read(file_name))
 
-]
+companies.each do |company|
+
+  cat = Category.find_by(title: company["Category"])
+
+  if !cat
+    cat = Category.create(title: company["Category"])
+  end
+
+  cat.organizations.create(
+    domain: company["Company"].downcase + '.com',
+    name: company["Company"]
+    )
+
+end
